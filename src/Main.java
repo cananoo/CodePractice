c0fd3d54ffbdb456f3d39efdec736a8d993cb7f5
root <2059997196@qq.com>
2023-10-16 06:46:39 +0800

    Word Break

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,83 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(singleNumber2(new int[]{1,1,3}));
+        System.out.println(wordBreak2("aaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaa",
+                Arrays.asList("aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa","ba")));
     }
+
+
+
+
+    /**
+     * Word Break
+     * @param s  字符串
+     * @param wordDict 字典
+     * @return 是否可以拆分       (动态规划思想)
+     */
+       /*
+        动态规划算法，dp[i]表示s前i个字符能否拆分
+        转移方程：dp[j] = dp[i] && check(s[i+1, j]);
+        check(s[i+1, j])就是判断i+1到j这一段字符是否能够拆分
+        其实，调整遍历顺序，这等价于s[i+1, j]是否是wordDict中的元素
+        这个举个例子就很容易理解。
+        假如wordDict=["apple", "pen", "code"],s = "applepencode";
+        dp[8] = dp[5] + check("pen")
+        翻译一下：前八位能否拆分取决于前五位能否拆分，加上五到八位是否属于字典
+        （注意：i的顺序是从j-1 -> 0哦~
+    */
+    public static boolean wordBreak2(String s, List<String> wordDict) {
+      Set<String> set = new HashSet<>();
+        for (String str : wordDict) {
+            set.add(str);
+        }
+        Boolean[] dp = new Boolean[s.length()+1];
+        dp[0] = true;
+        for (int j = 1; j < s.length() + 1; j++) {
+            for (int i = j-1; i >=0 ; i--) {
+                dp[j] = dp[i] && check(s.substring(i,j),set);
+                if (dp[j]) break;
+            }
+        }
+        return dp[s.length()];
+    }
+    private static boolean check(String s, Set<String> set){
+        return set.contains(s);
+    }
+
+
+    // 纯递归，时间复杂度过高
+        public static boolean wordBreak(String s, List<String> wordDict) {
+             Map<String,Integer> map = new HashMap<>();
+            for (String str : wordDict) {
+                map.put(str,1);
+            }
+            if (map.containsKey(s)){
+                return true;
+            }
+            return wordBreakHelp(s,map);
+        }
+    private static boolean wordBreakHelp(String s, Map<String,Integer> map) {
+        boolean flag = false;
+        int n = s.length();
+        List<String> strs = new ArrayList<>();
+        String word = "";
+        for (int i = 0; i < n ; i++) {
+            word += s.charAt(i);
+            if (map.containsKey(word)){
+                strs.add(word);
+            }
+        }
+        for (int i = 0; i < strs.size(); i++) {
+            String newStr = s.substring(strs.get(i).length());
+            boolean b = wordBreakHelp(newStr, map);
+            if (b){
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+
 
 
     /**
