b957d3a49f6ebab729b16fd66728f8b22ade8a59
root <2059997196@qq.com>
2024-02-05 12:01:25 +0800

    it's ok to complicate the problem

---
+++ src/RepeatPractice.java
@@ -0,0 +1,73 @@
+import java.math.BigInteger;
+import java.util.Stack;
+
+public class RepeatPractice {
+    public static void main(String[] args) {
+   // input  l1 : [9]
+
+        ListNode l1 = new ListNode(9);
+        // l2 : [1,9,9,9,9,9,9,9,9,9]
+
+        ListNode l2 = new ListNode(1,new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9))))))))));
+
+        ListNode res = addTwoNumbers(l1,l2);
+        while (res != null){
+            System.out.println(res.val);
+            res = res.next;
+        }
+
+    }
+
+
+      static  public class ListNode {
+          int val;
+          ListNode next;
+
+          ListNode() {
+          }
+
+          ListNode(int val) {
+              this.val = val;
+          }
+
+          ListNode(int val, ListNode next) {
+              this.val = val;
+              this.next = next;
+          }
+      }
+
+        public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            Stack<Integer> stack = new Stack<>();
+            while (l1 != null){
+                stack.push(l1.val);
+                l1 = l1.next;
+            }
+            StringBuilder temp1 = new StringBuilder();
+            while (!stack.isEmpty()){
+                temp1.append(stack.pop().toString());
+            }
+            while (l2 != null){
+                stack.push(l2.val);
+                l2 = l2.next;
+            }
+            StringBuilder temp2 = new StringBuilder();
+            while (!stack.isEmpty()){
+                temp2.append(stack.pop().toString());
+            }
+            BigInteger i = new BigInteger(temp1.toString());
+            BigInteger j = new BigInteger(temp2.toString());
+            BigInteger res = i.add(j);
+            String temp = String.valueOf(res);
+            for (int k = 0; k < temp.length(); k++) {
+                stack.push(Integer.valueOf(String.valueOf(temp.charAt(k))));
+            }
+            ListNode listNode = new ListNode(stack.pop());
+            ListNode t = listNode;
+            while (!stack.isEmpty()){
+                t.next = new ListNode(stack.pop());
+                t = t.next;
+            }
+            return listNode;
+        }
+    }
+
