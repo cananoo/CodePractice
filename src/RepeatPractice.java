e3ae4dbe3b135e7c069df8ca90619599be0d239d
root <2059997196@qq.com>
2024-03-13 15:00:40 +0800

    maxSubArray

--- src/RepeatPractice.java
+++ src/RepeatPractice.java
@@ -9,7 +9,15 @@ public class RepeatPractice {
     }
 
 
-
+    public static int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int pre = 0;
+        for (int num : nums) {
+            pre = Math.max(pre + num,num);
+            max = Math.max(max,pre);
+        }
+        return max;
+    }
 
     public  List<String> removeInvalidParentheses(String s) {
         int left = 0;
