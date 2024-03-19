d6cc3943a7fa2c29bc658b307c30bdda1b0f361a
root <2059997196@qq.com>
2024-03-19 13:40:43 +0800

    canPartition

--- src/RepeatPractice.java
+++ src/RepeatPractice.java
@@ -11,6 +11,35 @@ public class RepeatPractice {
 
 
 
+
+    public static boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) return false;
+        int target = sum / 2;
+        boolean[][] dp = new boolean[nums.length][target + 1];
+        if (nums[0] <= target){
+            dp[0][nums[0]] = true;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j <= target; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (nums[i] == j){
+                    dp[i][j] = true;
+                    continue;
+                }
+                if (nums[i] < j){
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
+                }
+            }
+        }
+        return dp[nums.length - 1][target];
+    }
+
+
+
     class LRUCache {
         int cap;
         public LRUCache(int capacity) {
