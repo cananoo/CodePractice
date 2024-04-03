d4d5cfe8c993ac41be210638552a86cc73d8cac5
root <2059997196@qq.com>
2024-04-03 17:52:52 +0800

    maxCoins

--- src/RepeatPractice.java
+++ src/RepeatPractice.java
@@ -12,6 +12,26 @@ public class RepeatPractice {
 
 
 
+    public static int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] coins = new int[n + 2];
+        coins[0] = 1;
+        coins[n + 1] = 1;
+        for (int i = 1; i < n + 1; i++) {
+            coins[i] = nums[i - 1];
+        }
+        int[][] dp = new int[n+2][n+2];
+        for (int len = 3; len <= n + 2; len++) {
+            for (int l = 0; l + len - 1 <= n + 1; l++) {
+                int r = l + len - 1;
+                for (int k = l + 1; k <= r - 1; k++) {
+                    dp[l][r] = Math.max(dp[l][r],dp[l][k] + dp[k][r] + coins[l] * coins[k] * coins[r]);
+                }
+            }
+        }
+        return dp[0][n + 1];
+    }
+
 
     public static int numTrees(int n) {
       if (n == 0) return 0;
