5ab143d97e7637836d68229ccea6ed9e09c8770c
root <2059997196@qq.com>
2024-04-07 22:26:26 +0800

    Algorithm 1 / 11 -- review blueCup

--- src/BuleBridgeCup.java
+++ src/BuleBridgeCup.java
@@ -6,6 +6,33 @@ public class BuleBridgeCup {
     }
 
 
+
+
+
+
+
+    public static int findIdx(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length -1;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }else if (arr[mid] > target) {
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
+
+
+
+
     /**
      * 七段码
      */
